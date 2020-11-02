7844636c892c03b24a4c5269c7432a1ebc5ac48f
Michael Wautier <wautiermichael@gmail.com>
2020-11-02 16:59:01 +0100

    refactored view and controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,10 +11,17 @@ class GamesController < ApplicationController
     @letters = params[:letters].split(' ')
     @word = params[:word].upcase
 
+    # JSON call
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     response = URI.open(url)
     @json_response = JSON.parse(response.read)
 
-    @letter_included = @word.chars.all? { |letter| @word.count(letter) <= @letters.count(letter) }
+    # Check the number of each letter of the word
+    @letter_included = @word.chars.all? do |letter|
+      @word.count(letter) <= @letters.count(letter)
+    end
+
+    # Increment current session score
+    session[:score] += @word.length**2
   end
 end
